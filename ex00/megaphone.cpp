bc0ef7e05b8d43e6eb676af2b985998c4857fe57
0ac_raf0dev <achrafennadiri@gmail.com>
2026-01-19 23:33:13 +0100

    update

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <cctype>
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    int i;
+    int j;
+    char c;
+
+    if (ac < 2)
+        return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 1);
+    i = 1;
+    while (av[i])
+    {
+        j = 0;
+        while(av[i][j])
+        {
+            c = std::toupper(av[i][j]);
+            std::cout << c;
+            j++;
+        }
+        i++;
+    }
+    std::cout << std::endl;
+    return (0);
+}
