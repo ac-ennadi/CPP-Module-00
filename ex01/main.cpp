5bf0562a763811bd3c790f8c260f7810d67a3d4c
0ac_raf0dev <achrafennadiri@gmail.com>
2026-01-20 10:39:07 +0100

    update

---
+++ ex01/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    std::string cmds; 
+    std::cout << "enter commends {ADD, SEARCH, EXIT}." << std::endl;
+    std::getline(std::cin, cmds);
+    while (1)
+    {
+        if (cmds == "ADD")
+        {
+
+        }
+        if (cmds == "SEARCH")
+        {
+
+        }
+        if (cmds == "EXIT")
+        {
+
+        }
+    }
+    return (0);
+}
